382d1bf9164cfcc03d663462b8c713bfcf9db772
Indu-Chandana <indu.99.ic@gmail.com>
2024-04-30 22:06:28 +0530

    Apr30 001 using Database Module inside the emp module. impt and use the databse service

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,20 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
-import { CreateEmployeeDto } from './dto/create-employee.dto';
-import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { Prisma } from '@prisma/client';
+
 
 @Controller('employees')
 export class EmployeesController {
-  constructor(private readonly employeesService: EmployeesService) {}
+  constructor(private readonly employeesService: EmployeesService) { }
 
   @Post()
-  create(@Body() createEmployeeDto: CreateEmployeeDto) {
+  create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) { // Prisma.EmployeeCreateInput --> this type create by prisma using we created model in prisma file.
     return this.employeesService.create(createEmployeeDto);
   }
 
   @Get()
-  findAll() {
-    return this.employeesService.findAll();
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.employeesService.findAll(role);
   }
 
   @Get(':id')
@@ -23,7 +23,7 @@ export class EmployeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
+  update(@Param('id') id: string, @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput) { // this type create by prisma using we created model in prisma file.
     return this.employeesService.update(+id, updateEmployeeDto);
   }
 
