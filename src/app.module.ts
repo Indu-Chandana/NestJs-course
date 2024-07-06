69f765ebd7aaa99adaf067a037b027f3d53162c7
Indu-Chandana <indu.99.ic@gmail.com>
2024-07-06 12:27:38 +0530

    July06 001 cors | api add to every endpoint | rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,35 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
 
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core'
+
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [
+    UsersModule, DatabaseModule, EmployeesModule,
+    ThrottlerModule.forRoot([
+      // { // we can use just this for all routes, It applies for all routes
+      // ttl: 60000, // ---->  3 req per 1min
+      // limit: 3 
+      // },
+      {
+        name: 'short', // after that we can use it in "employees.controller.ts" 
+        ttl: 1000, // 1 sec 3 req only
+        limit: 3
+      },
+      {
+        name: 'long',
+        ttl: 60000, // 1min 100 req only
+        limit: 100
+      }
+    ])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    { // for rate limit -> ThrottlerModule
+      provide: APP_GUARD, // for rate limit -> ThrottlerModule
+      useClass: ThrottlerGuard, // for rate limit -> ThrottlerModule
+    }// for rate limit -> ThrottlerModule
+  ],
 })
-export class AppModule {}
+export class AppModule { }
